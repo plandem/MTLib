ee139d658466a2022d7d1d5eb4726da4f1a21c35
plandem <plandem@gmail.com>
2015-06-06 12:26:07 +0800

    ...

--- MTData+SQLite/MTSQLiteDataFetchResult.h
+++ MTData+SQLite/MTSQLiteDataFetchResult.h
@@ -2,6 +2,8 @@
 // Created by Andrey on 04/06/15.
 // Copyright (c) 2015 Melatonin LLC. All rights reserved.
 //
+// Note: You can't iterate(objectEnumerator,enumerateObjectsUsingBlock) and use indexed(objectAtIndex,objectAtIndexedSubscript) access same time - result unpredictable.
+//
 
 #import "MTDataObject.h"
 
